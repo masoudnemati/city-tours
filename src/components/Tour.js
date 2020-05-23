f6dd4c0803b19a4bea6664a56e148929fdb81a7c
masoudNemati <masoud.nnr@gmail.com>
2020-05-23 17:57:45 +0430

    added function to info and delete icon now our app is completed!

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false,
+  };
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
   render() {
     const { id, city, img, name, info } = this.props.tour;
     const { removeTour } = this.props;
@@ -8,15 +16,18 @@ export default class Tour extends Component {
       <article className="rounded overflow-hidden shadow-lg mx-auto my-10">
         <div className="relative">
           <img src={img} alt="image" className="w-auto" />
-          <i className="far fa-times-circle absolute top-0 left-0 h-8 w-8 bg-red-400 pt-2 pr-2" />
+          <i
+            onClick={() => removeTour(id)}
+            className="far fa-times-circle absolute top-0 left-0 h-8 w-8 bg-red-400 pt-2 pr-2 cursor-not-allowed"
+          />
         </div>
         <div className="bg-gray-200 p-2 text-left">
           <h1 className="text-2xl"> {city}</h1>
           <h4 className="text-gray-600">{name}</h4>
-          <h5 className="pt-4">
+          <h5 className="pt-4 cursor-pointer" onClick={this.handleInfo}>
             info <i className="fas fa-caret-square-down" />
           </h5>
-          <p>{info}</p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
